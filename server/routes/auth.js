865f69aba3b3cee590b495c236ba6b6e22c602d7
mani <manideepsamsani12345@gmail.com>
2025-08-09 21:36:04 +0530

    Add input validation and test all routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,9 +1,13 @@
 // server/routes/auth.js
 const express = require("express");
 const router = express.Router();
-const { registerUser, loginUser } = require("../controllers/authController");
 
-router.post("/register", registerUser);
-router.post("/login", loginUser);
+const { registerUser, loginUser } = require("../controllers/authController");
+const { registerValidation, loginValidation } = require("../validators/authValidator");
+const validate = require("../middleware/validate");
+
+// Apply validation middleware before controller
+router.post("/register", registerValidation, validate, registerUser);
+router.post("/login", loginValidation, validate, loginUser);
 
 module.exports = router;
---
+++ server/validators/userValidator.js
@@ -0,0 +1,7 @@
+const { body } = require('express-validator');
+
+exports.updateProfileValidation = [
+  body('name').optional().notEmpty().withMessage('Name cannot be empty'),
+  body('email').optional().isEmail().withMessage('Valid email is required'),
+  body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters')
+];
